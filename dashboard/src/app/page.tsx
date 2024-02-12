a2b00717c75d97bf39f7cc56de3859559af3b7c4
Mate papava <matepapava123@gmail.com>
2024-02-12 17:49:05 +0400

    adding some ui

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,10 +1,11 @@
+import PageTitle from "@/components/PageTitle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-5 w-full">
 
-       Home Page
+       <PageTitle title="Dashboard" />
 
     </div>
   );
